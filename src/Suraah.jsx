40aae4b79b8fe65249f73873907759098ab3ef6d
Ahmadbyte <shariqueahmad011@gmail.com>
2023-09-11 15:35:38 +0530

    my first commit

---
+++ src/Suraah.jsx
@@ -0,0 +1,366 @@
+import React from "react";
+import "./suraah.css";
+
+function Surah() {
+  return (
+    <div>
+      <div className="head">
+      <a href="https://myislam.org/short-surahs/"><h1>My Priority Deen</h1></a>
+        <h2 className="desc">13 Short Surah to Learn !!</h2>
+      </div>
+      <p>
+        Learn very short Surah and earn Reward from Allah the Almighty.
+      </p>
+      <div className="surah">
+        <h2>1. SURAH AN-NASR :- </h2>
+       <img src="/images/1.jpg" alt="Surah AN-NASR" />
+        <p>
+          <strong>In English :- </strong>Iza jaa-a nas rullahi walfath,
+          <br />
+          Waraayta annasa yadkhuloonafee deeni Allahi afwaja,
+          <br />
+          Fa sab bih bihamdi rabbika was taghfir,<br/>innahu kaana tawwaaba
+        </p>
+        <p>
+          <strong>Translation :- </strong>
+          When the victory of Allah has come and the conquest,
+          <br />
+          And you see the people entering into the religion of Allah in
+          multitudes,
+          <br />
+          Then exalt [Him] with praise of your Lord and ask forgiveness of Him.{" "}
+          <br />
+          Indeed, He is ever Accepting of repentance.
+        </p>
+      </div>
+      <br />
+      <div className="surah">
+        <h2>2. SURAH AL-IKHLAS :- </h2>
+        <img src="/images/2.jpg" alt="Surah AN-NASR" />
+        <p>
+          <strong>In English :- </strong>Qul huwa Allahu ahad,
+          <br />
+          Allahu assamad,
+          <br />
+          Lam yalid wa lam yoolad,
+          <br/>
+          Wa lam yakul-lahu kufuwan ahad.
+        </p>
+        <p>
+          <strong>Translation :- </strong>
+          Say, “He is Allah, [who is] One,
+          <br />
+          Allah, the Eternal Refuge.
+          <br />
+          He neither begets nor is born
+          <br />
+          Nor is there to Him any equivalent.”
+        </p>
+      </div>
+      <br />
+      <div className="surah">
+        <h2>3. SURAH AL-ASR :- </h2>
+        <img src="/images/3.jpg" alt="Surah AN-NASR" />
+        <p>
+          <strong>In English :- </strong>Wal Asr,
+          <br />
+          Innal insaana lafee khusr,
+          <br />
+          Illa allatheena aamanu wa ‘amilus saali haati <br/>wa tawa saw bil haqqi <br/>
+          wa tawa saw bissabr.
+        </p>
+        <p>
+          <strong>Translation :- </strong>
+          By time,
+          <br />
+          Indeed, mankind is in loss,
+          <br />
+          Then exalt [Him] with praise of your Lord <br/>and ask forgiveness of Him.{" "}
+          <br />
+          Except for those who have believed and done righteous deeds and 
+          <br/>
+          advised each other to truth and advised each other to patience.
+        </p>
+      </div>
+      <br />
+      <div className="surah">
+        <h2>4. SURAH AL-KAWTHAR :- </h2>
+        <img src="/images/4.jpg" alt="Surah AN-NASR" />
+        <p>
+          <strong>In English :- </strong>Inna aAAtayna kaal kawthar,
+          <br />
+          Fa salli li rabbika wanhar,
+          <br />
+          Inna shani-aka huwa al-abtar.
+        </p>
+        <p>
+          <strong>Translation :- </strong>
+          Indeed, We have granted you, [O Muhammad], al-Kawthar.
+          <br />
+          So pray to your Lord and sacrifice [to Him alone].
+          <br />
+          Indeed, your enemy is the one cut off.
+        </p>
+      </div>
+      <br />
+      <div className="surah">
+        <h2>5. SURAH QURAYSH :- </h2>
+        <img src="/images/5.jpg" alt="Surah AN-NASR" />
+        <p>
+          <strong>In English :- </strong>Li-eelafi quraysh,
+          <br />
+          Eelafihim rihlata ashshita-iwassayf,
+          <br />
+          Fal y’abudu rabba haazal-bait
+          <br/>
+          Allazi at’amahum min ju’inw-wa-aamana hum min khawf.
+        </p>
+        <p>
+          <strong>Translation :- </strong>
+          For the accustomed security of the Quraysh-,
+          <br />
+          Their accustomed security [in] the caravan of winter and summer –,
+          <br />
+          Let them worship the Lord of this House,
+          <br />
+          Who has fed them, [saving them] from hunger and 
+          <br/>
+          made them safe, [saving them] from fear.
+        </p>
+      </div>
+      <br />
+      <div className="surah">
+        <h2>6. SURAH FALAQ :- </h2>
+        <img src="/images/6.jpg" alt="Surah AN-NASR" />
+        <p>
+          <strong>In English :- </strong>Qul a’uzoo bi rabbil-falaq,
+          <br />
+          Min sharri ma khalaq,
+          <br />
+          Wa min sharrin-naffaa-saati fil ‘uqad,
+          <br/>
+          Wa min shar ri haasidin iza hasad.
+        </p>
+        <p>
+          <strong>Translation :- </strong>
+          Say: I seek refuge with the Lord of the Dawn,
+          <br />
+          From the mischief of created things;,
+          <br />
+          From the mischief of Darkness as it overspreads;
+          <br />
+          From the mischief of those who practise secret arts;
+          <br/>
+          And from the mischief of the envious one as he practises envy.
+        </p>
+      </div>
+      <br />
+      <div className="surah">
+        <h2>7. SURAH MASAD :- </h2>
+        <img src="/images/7.jpg" alt="Surah AN-NASR" />
+        <p>
+          <strong>In English :- </strong>Tab bat yadaa abee Lahabinw-wa tabb,
+          <br />
+          Maa aghna ‘anhu maaluhu wa ma kasab,
+          <br />
+          Sa yas laa naran zaata lahab
+          <br/>
+          Wam ra-atuhu hamma latal-hatab,
+          <br/>
+          Fee jeediha hab lum mim-masad.
+        </p>
+        <p>
+          <strong>Translation :- </strong>
+          May the hands of Abu Lahab be ruined, and ruined is he.
+          <br/>
+          His wealth will not avail him or that which he gained.
+          <br/>
+          He will [enter to] burn in a Fire of [blazing] flame
+          <br/>
+          And his wife [as well] – the carrier of firewood.
+          <br/>
+          Around her neck is a rope of [twisted] fiber.
+        </p>
+      </div>
+      <br />
+      <div className="surah">
+        <h2>8. SURAH QADR :- </h2>
+        <img src="/images/8.jpg" alt="Surah AN-NASR" />
+        <p>
+          <strong>In English :- </strong>Innaa anzalnaahu fee lailatil qadr,
+          <br />
+          Wa maa adraaka ma lailatul qadr,
+          <br />
+          Lailatul qadri khairum min alfee shahr,
+          <br/>
+          Tanaz zalul malaa-ikatu war roohu feeha 
+          <br/>
+          bi izni-rabbihim min kulli amr,
+          <br/>
+          Salaamun hiya hattaa mat la’il fajr.
+        </p>
+        <p>
+          <strong>Translation :- </strong>
+          Indeed, We sent the Qur’an down during the Night of Decree.
+          <br />
+          And what can make you know what is the Night of Decree?
+          <br />
+          The Night of Decree is better than a thousand months.
+          <br />
+          The angels and the Spirit descend therein 
+          <br/>
+          by permission of their Lord for every matter.
+          <br/>
+          Peace it is until the emergence of dawn.
+        </p>
+      </div>
+      <br />
+      <div className="surah">
+        <h2>9. SURAH KAFIRUN :- </h2>
+        <img src="/images/9.jpg" alt="Surah AN-NASR" />
+        <p>
+          <strong>In English :- </strong>Qul yaa-ai yuhal kaafiroon,
+          <br />
+          Laa a’budu ma t’abudoon, <br/>Wa laa antum ‘aabidoona maa a’bud,
+          <br />
+          Wa laa ana ‘abidum maa ‘abattum,
+          <br/>
+          Wa laa antum ‘aabidoona ma a’bud, <br/>Lakum deenukum wa liya deen.
+        </p>
+        <p>
+          <strong>Translation :- </strong>
+          Say, “O disbelievers, I do not worship what you worship.
+          <br />
+          Nor are you worshipers of what I worship.
+          <br />
+          Nor will I be a worshipper of what you worship.
+          <br />
+          Nor will you be worshippers of what I worship.
+          <br/>
+          For you is your religion, and for me is my religion.”
+        </p>
+      </div>
+      <br />
+      <div className="surah">
+        <h2>10. SURAH AN-NAS :- </h2>
+        <img src="/images/10.jpg" alt="Surah AN-NASR" />
+        <p>
+          <strong>In English :- </strong>Qul a’uzu birabbin naas,
+          <br />
+          Malikin naas, Ilaahin naas,
+          <br />
+          Min sharril was waasil khannaas,
+          <br/>
+          Al lazee yuwas wisu fee sudoorin naas
+          <br/>
+          Minal jinnati wan naas.
+        </p>
+        <p>
+          <strong>Translation :- </strong>
+          Say, “I seek refuge in the Lord of mankind,
+          <br />
+          The Sovereign of mankind. The God of mankind,
+          <br />
+          From the evil of the retreating whisperer,
+          <br />
+          Who whispers [evil] into the breasts of mankind – 
+          <br/>
+          From among the jinn and mankind.”
+        </p>
+      </div>
+      <br />
+      <div className="surah">
+        <h2>11. SURAH AL-FIL :- </h2>
+        <img src="/images/11.jpg" alt="Surah AN-NASR" />
+        <p>
+          <strong>In English :- </strong>Alam tara kaifa fa’ala rabbuka bi ashaabil feel,
+          <br />
+          Alam yaj’al kai dahum fee tad leel,
+          <br />
+          Wa arsala ‘alaihim tairan abaabeel,
+          <br/>
+          Tar meehim bi hi jaaratim min sij jeel,
+          <br/>
+          Faja ‘alahum ka’asfim m’akool.
+        </p>
+        <p>
+          <strong>Translation :- </strong>
+          Have you not considered, [O Muhammad], 
+          <br/>
+          how your Lord dealt with the companions of the elephant?
+          <br />
+          Did He not make their plan into misguidance?
+          <br />
+          And He sent against them birds in flocks,
+          <br />
+          Striking them with stones of hard clay,
+          <br/>
+          And He made them like eaten straw. 
+        </p>
+      </div>
+      <br />
+      <div className="surah">
+        <h2>12. SURAH MAUN :- </h2>
+        <img src="/images/12.jpg" alt="Surah AN-NASR" />
+        <p>
+          <strong>In English :- </strong>Ara-aital lazee yu kazzibu bid deen,
+          <br/>
+          Fa zaalikal lazi yadu’ul-yateem,
+          <br />
+          Wa la ya huddu ‘alaa ta’amil miskeen, Fa wai lul-lil mu salleem,
+          <br />
+          Al lazeena hum ‘an salaatihim sahoon,
+          <br/>
+          Al lazeena hum yuraa-oon, Wa yamna’oonal ma’oon.
+        </p>
+        <p>
+          <strong>Translation :- </strong>
+          Have you seen the one who denies the Recompense?
+          <br />
+          For that is the one who drives away the orphan,
+          <br />
+          And does not encourage the feeding of the poor.
+          <br />
+          So woe to those who pray, [But] who are heedless of their prayer –
+          <br/>
+          Those who make show [of their deeds], And withhold [simple] assistance.
+        </p>
+      </div>
+      <br />
+      <div className="surah">
+        <h2>13. SURAH AT-TIN :- </h2>
+        <img src="/images/13.jpg" alt="Surah AN-NASR" />
+        <p>
+          <strong>In English :- </strong>Wat teeni waz zaitoon,<br/> Wa toori sineen, <br/>Wa haazal balad-il ameen,
+          <br />
+          Laqad khalaqnal insaana fee ahsani taqweem, <br/>Thumma ra dad naahu asfala saafileen,
+          <br />
+          Ill-lal lazeena aamanoo wa ‘amilus saalihaati;<br/> falahum ajrun ghairu mamnoon,
+          <br/>
+          Fama yu kaz zibuka b’adu bid deen, <br/>Alai sal laahu bi-ahkamil haakimeen.
+        </p>
+        <p>
+          <strong>Translation :- </strong>
+          By the fig and the olive,<br/> And [by] Mount Sinai, <br/>And [by] this secure city [Makkah],
+          <br />
+          We have certainly created man in the best of stature; <br/>Then We return him to the lowest of the low,
+          <br />
+          Except for those who believe and do righteous deeds, <br/>for they will have a reward uninterrupted.
+          <br />
+          So what yet causes you to deny the Recompense? 
+          <br/>
+          Is not Allah the most just of judges?
+        </p>
+      </div>
+      <br />
+      <footer className="footer">
+        <h5>@Copyright(2023) Reserved by Sharique Ahmad  </h5>
+        
+        <a href="www.instagram.com"><img src="/images/insta.png" alt="no"></img></a>
+      <a href="https://github.com/Ahmadbyte"><img src="/images/git.png" alt="no"></img></a>
+      <a href="https://www.linkedin.com/in/sharique-ahmad-8229221b3/"><img src="/images/linkdin.png" alt="no"></img></a>
+      </footer> 
+    </div>   
+  );
+}
+export default Surah;
